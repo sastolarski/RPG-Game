4dd7c24af366c91441da3cf28fa3079e3041bef7
sastolarski <sastolarski@gmail.com>
2018-04-27 15:32:57 -0500

    stuff

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,40 @@
 //objects for each character? with hit points, attack power, and counter attack power, and function for attacking and function for counter attack.
 
 $(document).ready(function(){
-    
+
 var characters = {
 
-    "luke" : {
+    luke : {
         name : "luke",
         hitpoints : 100,
         attack: 6,
         counterAttack : 15,
+
     },
 
-    "hanSolo" : {
+    hanSolo : {
         name : "Han Solo",
         hitpoints : 140,
         attack : 12,
         counterAttack : 10,
     },
 
-    "greedo" : {
+    greedo : {
         name : "Greedo",
         hitpoints : 180,
         attack : 8,
         counterAttack : 20,
     },
 
-    "bobaFett" : {
+    bobaFett : {
         name : "Boba Fett",
         hitpoints : 120,
         attack : 10,
         counterAttack : 12
     }
 };
+
+
 
 
 
